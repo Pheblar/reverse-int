a36b6fd078feba7d5d720aa83f62c6bf8dd0475b
Pheblar <roman.shor@solinstruments.com>
2021-02-03 13:52:11 +0300

    feat: add fix to minus

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 module.exports = function reverse (n) {
   n = String(n);
-  let a = n.split().reverse().join();
+  let a = n.split(''); 
+  if (a[0] === '-') {
+    a.splice(0,1);
+  }
+  a = a.reverse().join('');
   a = Number(a)
   return a;
 }
